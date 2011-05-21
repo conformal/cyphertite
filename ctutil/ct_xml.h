47a4ad4368aea236e44c57982af8e95bb32a993a
Ken Westerback <krw@conformal.com>
2011-05-21 03:16:06 +0000

    Define a version string for each command that didn't have one. The new ones are not used yet in building XML.

--- ctutil/ct_xml.h
+++ ctutil/ct_xml.h
@@ -6,8 +6,17 @@
 
 /* XML transactions for Cyphertite MD. */
 
+#define CT_MD_OPEN_CREATE_VERSION	"V1"
 extern struct xmlsd_v_elem xe_ct_md_open_create[];
+
+#define CT_MD_OPEN_READ_VERSION		"V1"
 extern struct xmlsd_v_elem xe_ct_md_open_read[];
+
+#define CT_MD_CLOSE_VERSION		"V1"
 extern struct xmlsd_v_elem xe_ct_md_close[];
+
+#define CT_MD_LIST_VERSION		"V1"
 extern struct xmlsd_v_elem xe_ct_md_list[];
+
+#define CT_MD_DELETE_VERSION		"V1"
 extern struct xmlsd_v_elem xe_ct_md_delete[];
