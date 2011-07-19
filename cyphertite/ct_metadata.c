4fce1d71eae69e98c76fa80c2fb5de917d64a126
Owain Ainsworth <oga@conformal.com>
2011-07-19 22:25:36 +0000

    fts means you must include sys/types.h and sys/stat.h

--- cyphertite/ct_metadata.c
+++ cyphertite/ct_metadata.c
@@ -15,6 +15,9 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include <inttypes.h>
 #include <stdlib.h>
 #include <dirent.h>
