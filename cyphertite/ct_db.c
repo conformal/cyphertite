b500611c020a0262151d08f6d6fc375f6097488a
Dale Rahn <drahn@conformal.com>
2012-03-22 08:21:57 -0500

    Update Copyrights where the file has been modified

--- cyphertite/ct_db.c
+++ cyphertite/ct_db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2010 Conformal Systems LLC <info@conformal.com>
+ * Copyright (c) 2010-2012 Conformal Systems LLC <info@conformal.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
