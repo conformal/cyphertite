aed63863ed28cac336242b9757377e1cfff61835
Owain G. Ainsworth <oga@conformal.com>
2013-03-27 17:39:58 +0000

    Don't loop on reconnect if we fatal in just the wrong place.

--- libcyphertite/ct_queue.c
+++ libcyphertite/ct_queue.c
@@ -1357,6 +1357,18 @@ ct_process_write(void *vctx)
 	void			*data;
 	int			 nchunks;
 
+	/*
+	 * If we fatalled. just state transiition everything and don't
+	 * send any more. This should complete everything even if we
+	 * need to reconnect.
+	 */
+	if (state->ct_dying != 0) {
+		while ((trans = ct_dequeue_write(state)) != NULL) {
+			ct_queeu_transfer(state, trans);
+		}
+		return;
+	}
+
 	/* did we idle out? */
 	if (state->ct_reconnect_pending) {
 		if (ct_reconnect_internal(state) != 0)
